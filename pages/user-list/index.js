e45ee1497d33dddf6121b751c94be3d0ec220596
henrique2601 <henrique_2601@hotmail.com>
2019-07-27 14:33:51 -0300

    2 files modified, 1 added

--- pages/user-list/index.js
+++ pages/user-list/index.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
 
 export default class UserList extends Component {
+
+  state = {
+    users: []
+  }
+
+  componentDidMout() {
+    this.loadUsers();
+  }
+
   render() {
     return(
       <>
@@ -8,5 +18,10 @@ export default class UserList extends Component {
         <p>Teste de conteudo</p>
       </>
     )
+  }
+
+  loadUsers = async () => {
+    const res = await api.get(`/users?per_page=10?page=1`);
+    this.setState({ users: res.data.data });
   }
 }
