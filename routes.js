b42034320c8776915aeb6e5e37eb1355b0a100f3
henrique2601 <henrique_2601@hotmail.com>
2019-07-27 13:55:52 -0300

    1 file modified

--- routes.js
+++ routes.js
@@ -8,13 +8,13 @@ import {
 import UserList from './pages/user-list';
 import UserDetail from './pages/user-detail';
 
-const Routes = () => {
+const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={UserList} />
       <Route path="/users/:id" component={UserDetail} />
     </Switch>
   </BrowserRouter>
-}
+)
 
 export default Routes;
