8406f1044cbac1774d94fc345195b94a98da31fa
henrique2601 <henrique_2601@hotmail.com>
2019-07-27 15:59:05 -0300

    1 file modified

--- pages/user-list/index.js
+++ pages/user-list/index.js
@@ -6,7 +6,9 @@ import './styles.css';
 export default class UserList extends Component {
 
   state = {
-    users: []
+    users: [],
+    userInfo: {},
+    page: 1
   }
 
   componentDidMount() {
@@ -28,16 +30,34 @@ export default class UserList extends Component {
       <div className="user-list">
         { users.map(user => this.rowUser(user)) }
         <div className="actions">
-          <button>Anterior</button>
-          <button>Proximo</button>
+          <button onclick={this.prevpage}>Anterior</button>
+          <button onclick={this.nextPage}>Proximo</button>
         </div>
       </div>
     );
   }
 
-  loadUsers = async () => {
-    const res = await api.get(`/users?per_page=3?page=1`);
-    this.setState({ users: res.data.data });
+  loadUsers = async (page = 1) => {
+    const res = await api.get(`/users?per_page=3?page=${page}`);
+    const { data, ...userInfo } = res.data;
+
+    this.setState({ users: data, userInfo, page });
+  }
+
+  prevpage = () => {
+    const {page, userInfo} = this.state 
+    if (page === 1) return;
+    
+    const pageNumber = page + 1;
+    this.loadUsers(pageNumber);
+  }
+
+  nextPage = () => {
+    const {page, userInfo} = this.state 
+    if (page === userInfo.total_pages) return;
+    
+    const pageNumber = page -1;
+    this.loadUsers(pageNumber);
   }
 }
 
