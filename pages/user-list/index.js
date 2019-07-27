8ddf6448c413477116f4288bd4aa5a829ba3d2a9
henrique2601 <henrique_2601@hotmail.com>
2019-07-27 14:18:58 -0300

    1 file modified

--- pages/user-list/index.js
+++ pages/user-list/index.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class UserList extends Component {
   render() {
     return(
-      <h1>User List</h1>
+      <>
+        <h1>User List</h1>
+        <p>Teste de conteudo</p>
+      </>
     )
   }
 }
